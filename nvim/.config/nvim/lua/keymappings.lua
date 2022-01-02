c909cb034f053f4d979c8d13594d23777065ea95
Andy Mroczkowski <a@mrox.co>
2022-01-02 02:13:15 -0500

    cleanup and comment

--- nvim/.config/nvim/lua/keymappings.lua
+++ nvim/.config/nvim/lua/keymappings.lua
@@ -4,16 +4,17 @@ local vimp = require('vimp')
 utils.map('n', '<C-l>', '<cmd>noh<CR>') -- Clear highlights
 utils.map('i', 'jk', '<Esc>')           -- jk to escape
 
-if(vim.g.vscode) then
+-- TODO: figure this out - says the keys were already mapped ??
+-- if(vim.g.vscode) then
 
-    -- utils.map('i', '<C-/>', '<Plug>VSCodeCommentarygv')
-    -- utils.map('n', '<C-/>', '<Plug>VSCodeCommentaryLine')
+--     -- utils.map('i', '<C-/>', '<Plug>VSCodeCommentarygv')
+--     -- utils.map('n', '<C-/>', '<Plug>VSCodeCommentaryLine')
 
-    vimp.xmap('<C-/>', '<Plug>VSCodeCommentarygv')
-    vimp.nmap('<C-/>', '<Plug>VSCodeCommentaryLine')
+--     vimp.xmap('<C-/>', '<Plug>VSCodeCommentarygv')
+--     vimp.nmap('<C-/>', '<Plug>VSCodeCommentaryLine')
 
-    -- vimp.xmap('gc', '<Plug>VSCodeCommentary')
-    -- vimp.nmap('gc', '<Plug>VSCodeCommentary')
-    -- vimp.omap('gc', '<Plug>VSCodeCommentary')
-    -- vimp.nmap('gcc', '<Plug>VSCodeCommentaryLine')
-end
+--     -- vimp.xmap('gc', '<Plug>VSCodeCommentary')
+--     -- vimp.nmap('gc', '<Plug>VSCodeCommentary')
+--     -- vimp.omap('gc', '<Plug>VSCodeCommentary')
+--     -- vimp.nmap('gcc', '<Plug>VSCodeCommentaryLine')
+-- end
