9ac9d25b7d907ef60cc8d054d768fdef6cbd691a
Andy Mroczkowski <a@mrox.co>
2022-01-02 14:41:03 -0500

    Implement some new settings
    via https://github.com/brainfucksec/neovim-lua/blob/main/nvim/lua/settings.lua

--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -1,7 +1,30 @@
+-- 
+-- began to pull stuff from https://github.com/brainfucksec/neovim-lua/blob/main/nvim/lua/settings.lua#L8
+
 local utils = require('utils')
 
 local cmd = vim.cmd
+local opt = vim.opt
 local indent = 4
+
+-----------------------------------------------------------
+-- General
+-----------------------------------------------------------
+opt.mouse = 'a'
+opt.clipboard = 'unnamed,unnamedplus'
+opt.swapfile = false
+
+-----------------------------------------------------------
+-- Neovim UI
+-----------------------------------------------------------
+opt.number = true             -- show line number
+opt.relativenumber = true     -- show relative line number
+opt.showmatch = true          -- highlight matching parenthesis
+opt.ignorecase = true         -- ignore case letters when search
+opt.smartcase = true          -- ignore lowercase for the whole pattern
+opt.linebreak = true          -- wrap on word boundary
+
+-----------------------------------------------------------
 
 cmd 'syntax enable'
 cmd 'filetype plugin indent on'
@@ -10,16 +33,11 @@ utils.opt('b', 'shiftwidth', indent)
 utils.opt('b', 'smartindent', true)
 utils.opt('b', 'tabstop', indent)
 utils.opt('o', 'hidden', true)
-utils.opt('o', 'ignorecase', true)
 utils.opt('o', 'scrolloff', 4 )
 utils.opt('o', 'shiftround', true)
-utils.opt('o', 'smartcase', true)
 utils.opt('o', 'splitbelow', true)
 utils.opt('o', 'splitright', true)
 utils.opt('o', 'wildmode', 'list:longest')
-utils.opt('w', 'number', true)
-utils.opt('w', 'relativenumber', true)
-utils.opt('o', 'clipboard','unnamed,unnamedplus')
 
 -- Highlight on yank
 vim.cmd 'au TextYankPost * lua vim.highlight.on_yank {on_visual = false}'
